a6a1327e12a668676b848d903e6a8a221d3be493
Tulio-Rangel <tjrn20@hotmail.com>
2022-07-03 11:44:51 -0500

    Reto 2 resuelto

---
+++ Reto2/reto2/src/Peaje.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class Peaje {
+    //Atributos
+    private String[] filaCoches;
+    private String[] cochesFlyPass;
+    private boolean estadoPeaje = true;
+    private int cantidadCochesAtendidos = 1;
+    private int cocheEnAtencion = 0;
+
+    //Constructor
+    public Peaje(String[] filaCoches) {
+        this.filaCoches = filaCoches;
+        this.cochesFlyPass = new String[filaCoches.length];
+        for (int i = 0; i < filaCoches.length; i++) {
+            this.cochesFlyPass[i] = " ";
+        }
+    }
+
+    //Métodos
+    public void proximoCoche() {
+        if (estadoPeaje) {
+            cocheEnAtencion++;
+            cantidadCochesAtendidos++;
+        }
+    }
+
+    public void agregarCocheFlyPass(){
+        String cocheABuscar = filaCoches[cocheEnAtencion];
+        for(String coche:cochesFlyPass){
+            if(coche.equals(cocheABuscar)){
+                break;
+            }
+            if(" ".equals(coche)){
+                coche = cocheABuscar;
+            }
+        }
+    }
+
+    public void cambiarEstadoPeaje() {
+        if (estadoPeaje) {
+            estadoPeaje = false;
+        } else {
+            estadoPeaje = true;
+        }
+    }
+
+    //Getters y Setters
+    public String[] getFilaCoches() {
+        return filaCoches;
+    }
+
+    public void setFilaCoches(String[] filaCoches) {
+        this.filaCoches = filaCoches;
+    }
+
+    public String[] getCochesFlyPass() {
+        return cochesFlyPass;
+    }
+
+    public void setCochesFlyPass(String[] cochesFlyPass) {
+        this.cochesFlyPass = cochesFlyPass;
+    }
+
+    public boolean isEstadoPeaje() {
+        return estadoPeaje;
+    }
+
+    public void setEstadoPeaje(boolean estadoPeaje) {
+        this.estadoPeaje = estadoPeaje;
+    }
+
+    public int getCantidadCochesAtendidos() {
+        return cantidadCochesAtendidos;
+    }
+
+    public void setCantidadCochesAtendidos(int cantidadCochesAtendidos) {
+        this.cantidadCochesAtendidos = cantidadCochesAtendidos;
+    }
+
+    public int getCocheEnAtencion() {
+        return cocheEnAtencion;
+    }
+
+    public void setCocheEnAtencion(int cocheEnAtencion) {
+        this.cocheEnAtencion = cocheEnAtencion;
+    }
+}
